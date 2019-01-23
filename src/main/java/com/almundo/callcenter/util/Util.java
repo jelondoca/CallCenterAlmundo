e17cde9cddde1851d214fcb2f6f468110fb054af
jelondoca <jelondoca@gmail.com>
2019-01-23 09:25:10 -0500

    Update Util.java
    Mejora en la utilidad para poder crear el Random de forma eficiente

--- src/main/java/com/almundo/callcenter/util/Util.java
+++ src/main/java/com/almundo/callcenter/util/Util.java
@@ -6,13 +6,13 @@ public class Util {
 
     private static final int MIN_TIME = 5;
     private static final int MAX_TIME = 10;
+    private static final Random randomTime = new Random();
 
     Util() {
         throw new IllegalAccessError("Utility class");
     }
 
     public static int getRandomTimeCall() {
-        Random randomTime = new Random();
         return randomTime.nextInt((MAX_TIME - MIN_TIME) + 1) + MIN_TIME;
     }
 }
